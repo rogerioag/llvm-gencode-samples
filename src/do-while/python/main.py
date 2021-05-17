2dc8242a30c48b7a79d01d0f37ada9cfa2673add
hmarcuzzo <henrique.souza.m06@gmail.com>
2021-05-16 23:11:24 -0300

    Exemplo de soma e arrumando exemplo de do-while

--- src/do-while/python/main.py
+++ src/do-while/python/main.py
@@ -49,25 +49,35 @@ auxa = ir.Constant(ir.IntType(32), 1)
 builder.store(auxi, i)
 builder.store(auxa, a)
 
+loop = builder.append_basic_block('loop')
+lopp_val = builder.append_basic_block('loop_val')
+loop_end = builder.append_basic_block('loop_end')
+
+# Pula para o laço do loop
+builder.branch(loop)
+
+# Posiciona no inicio do bloco do loop e define o que o loop irá executar
+builder.position_at_end(loop)
+builder.store(ir.Constant(ir.IntType(32), 5), a)
+soma = builder.add(builder.load(i), ir.Constant(ir.IntType(32), 1))
+builder.store(i, soma)
+
+# Pula para o laço de validação
+builder.branch(lopp_val)
+
+# Posiciona no inicio do bloco de validação e define o que o loop de validação irá executar
+builder.position_at_end(lopp_val)
+
 i_loaded = builder.load(i, 'i', align=4)
 aux_number = ir.Constant(ir.IntType(32), 1024)
 
-#  IRBuilder.icmp_signed(cmpop, lhs, rhs, name='')
+# Gera a expressão de comparação
 expression = builder.icmp_signed('<', i_loaded, aux_number, name='expression')
 
-loop = builder.append_basic_block('loop')
-loop_end = builder.append_basic_block('loop_end')
+# Compara se a expressão é verdadeira ou não, caso for pula para o bloco do loop, caso contrário pula para o bloco do loop end
+builder.cbranch(expression, loop, loop_end)
 
-builder.cbranch(builder.not_(expression), loop, loop_end)
-builder.position_at_end(loop)
-
-builder.position_at_end(loop)
-builder.store(ir.Constant(ir.IntType(32), 5), a)
-soma = builder.add(builder.load(), ir.Constant(ir.IntType(32), 1))
-builder.store(i, soma)
-builder.branch(loop)
-
-builder.cbranch(builder.not_(expression), loop_block, loop_end)
+# Posiciona no inicio do bloco do fim do loop (saída do laço) e define o que o será executado após o fim (o resto do programa)  
 builder.position_at_end(loop_end)
 
 # Cria um salto para o bloco de saída
--- src/sum/python/main.py
+++ src/sum/python/main.py
@@ -4,14 +4,59 @@ from llvmlite import ir
 Este módulo contém uma função main e criação de um array unidimensional como variável global e outro como local.
 Será gerado um código em LLVM como este em C:
 
-<<Colocar o Código do Exemplo de Referência.>>
+int sum(int x, int y) {
+    return (x + y)
+}
+
+int main(){
+	int a = 12;
+	int b = 30;
+	int res = sum(a,b);
+	printf("res: %d\n", res);
+	return 0;
+}
 
 '''
 
 # Cria o módulo.
 module = ir.Module('meu_modulo.bc')
 
+# Cria o cabeçalho da função soma
+t_soma = ir.FunctionType(ir.IntType(32), [ir.IntType(32), ir.IntType(32)])
+soma = ir.Function(module, t_soma, 'soma')
+soma.args[0].name = 'a'
+soma.args[1].name = 'b'
 
+# Cria o corpo da função soma
+entryBlock = soma.append_basic_block('entry')
+builder = ir.IRBuilder(entryBlock)
+
+res = builder.add(soma.args[0], soma.args[1])
+builder.ret(res)
+
+# Cria o cabeçalho da função main
+t_main = ir.FunctionType(ir.IntType(32), [])
+main = ir.Function(module, t_main, 'main')
+
+# Cria o corpo da função main
+entryBlock = main.append_basic_block('entry')
+builder = ir.IRBuilder(entryBlock)
+
+# int a = 12;
+a = builder.alloca(ir.IntType(32), name='a')
+builder.store(ir.Constant(ir.IntType(32), 12), a)
+
+# int b = 30;
+b = builder.alloca(ir.IntType(32), name='b')
+builder.store(ir.Constant(ir.IntType(32), 30), b)
+
+# int res = soma(a,b);
+res = builder.alloca(ir.IntType(32), name='res')
+call = builder.call(soma, [builder.load(a), builder.load(b)])
+builder.store(call, res)
+
+# return res;
+builder.ret(res)
 
 arquivo = open('meu_modulo.ll', 'w')
 arquivo.write(str(module))
