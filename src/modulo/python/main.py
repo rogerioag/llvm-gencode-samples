893de6849fb955b972fecb8eec9b5cd5436b49dd
Rogério A. Gonçalves <rogerio.rag@gmail.com>
2023-11-09 17:05:03 -0300

    Updating samples.

--- src/modulo/python/main.py
+++ src/modulo/python/main.py
@@ -8,12 +8,18 @@ Será gerado um código em LLVM como este em C:
 
 '''
 
-
-
-
+# Código de Inicialização.
+llvm.initialize()
+llvm.initialize_all_targets()
+llvm.initialize_native_target()
+llvm.initialize_native_asmprinter()
 
 # Cria o módulo.
 module = ir.Module('meu_modulo.bc')
+module.triple = llvm.get_process_triple()
+target = llvm.Target.from_triple(module.triple)
+target_machine = target.create_target_machine()
+module.data_layout = target_machine.target_data
 
 
 # Salva o Módulo.
